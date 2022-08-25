9d38a3666e4262871b355373a7fdaae71fc2d6f9
ParkourNinjaKitty <68483205+ParkourNinjaKitty@users.noreply.github.com>
2022-08-25 16:28:24 -0700

    Delete Animation Player Only directory

--- Animation Player Only/Player/Player.gd
+++
@@ -1,6 +0,0 @@
-extends KinematicBody2D
-func _unhandled_key_input(_event):
-	$AnimationPlayer.play("Jump")
-func _on_Hurtbox_body_entered(_body):
-	Ui.get_node("Score").text = "-2"
-	get_tree().reload_current_scene()
--- Animation Player Only/World/Obstacle.gd
+++
@@ -1,5 +0,0 @@
-extends RigidBody2D
-func _on_VisibilityNotifier2D_screen_exited():
-	global_position = Vector2(384, rand_range(0, -64))
-	linear_velocity.x -= 8
-	Ui.get_node("Score").text = str(int(Ui.get_node("Score").text)+1)
